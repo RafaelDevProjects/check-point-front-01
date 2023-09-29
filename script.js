78bd397e5d10d1289228d010e2de80ea80babdfd
Rafael De Almeida <124459052+RafaelDevProjects@users.noreply.github.com>
2023-09-29 08:43:44 -0300

    final

---
+++ script.js
@@ -0,0 +1,9 @@
+var nome = "Rafael";
+var idade = 19;
+var status = true;
+var valorProduto = 10.50;
+var listaTimes = ["São paulo", "Flamengo", "Santos"]
+
+console.log(nome)
+console.log(idade)
+console.log(status)

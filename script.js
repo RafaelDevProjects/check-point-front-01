59b2d534fa6d7d2f74c56932970572f635bd8c3b
Rafael De Almeida <124459052+RafaelDevProjects@users.noreply.github.com>
2023-09-29 08:47:48 -0300

    finalizando

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 var nome = "Rafael";
 var idade = 19;
 var status = true;
-var valorProduto = 10.50;
+var valorProduto = 10.50
 var listaTimes = ["São paulo", "Flamengo", "Santos"]
 
-console.log(nome)
-console.log(idade)
-console.log(status)
+console.log(nome);
+console.log(idade);
+console.log(status);
+console.log(valorProduto);
+console.log(listaTimes);
+
+soma = 2 + 2;
+subtração = 2 - 2;
+divisão = 2/2;
+multiplicação = 2 * 2;
+
+console.log('Soma:'+soma);
+console.log('Subtração:'+subtração);
+console.log('Divisão:'+divisão);
+console.log('Multiplicação:'+multiplicação);
+
+
